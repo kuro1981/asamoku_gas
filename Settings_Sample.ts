675c57be3becde16d1c004ecaf0a0805f6e15719
akaikaze <akaikaze.monokuro@gmail.com>
2021-11-08 09:19:16 +0900

    add Settings_Sample

---
+++ Settings_Sample.ts
@@ -0,0 +1,4 @@
+// Copy and Rename to Settings.ts and modify slack_hook_url
+var slack_hook_url = "slack inccoming web hook url"
+// if you want to use skype, you turn use_skype to true.
+var use_skype = false
